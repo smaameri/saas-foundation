bd794e6bf5405370478901068536465c359d9a23
Sami Maameri <ssmaameri@gmail.com>
2026-07-05 14:16:45 +0530

    setup prettier, eslint and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,25 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettier,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "generated/**",
   ]),
 ]);
 
