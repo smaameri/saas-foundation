bdd74de44aad5b9e4f5fdce47d0d0c81e8f8aa29
Sami Maameri <ssmaameri@gmail.com>
2026-07-07 13:14:19 +0530

    use new column headers

--- src/app/(web)/admin/api-keys/columns.tsx
+++ src/app/(web)/admin/api-keys/columns.tsx
@@ -2,7 +2,7 @@
 
 import type { ColumnDef } from "@tanstack/react-table";
 import type { ApiKey } from "@/services/api/types/apiKey";
-import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header";
+import { DataTableColumnHeader } from "@/components/connected-data-table/column-header";
 import { DeleteApiKeyButton } from "@/app/(web)/admin/api-keys/_components/delete-api-key-button";
 
 export const columns: ColumnDef<ApiKey>[] = [
