c36e65e805c8d53677c6e88b24ffd200c3339517
Sami Maameri <ssmaameri@gmail.com>
2026-07-12 13:05:18 +0530

    fix app sidebar highlighting on nested paths

--- src/components/layout/nav-group.tsx
+++ src/components/layout/nav-group.tsx
@@ -142,9 +142,10 @@ function SidebarMenuCollapsedDropdown({ item, href }: { item: NavCollapsible; hr
 
 function checkIsActive(href: string, item: NavItem, mainNav = false) {
   return (
-    href === item.url || // /endpint?search=param
-    href.split("?")[0] === item.url || // endpoint
-    !!item?.items?.filter((i) => i.url === href).length || // if child nav is active
-    (mainNav && href.split("/")[1] !== "" && href.split("/")[1] === item?.url?.split("/")[1])
+    href === item.url ||
+    href.split("?")[0] === item.url ||
+    !!item?.items?.filter((i) => i.url === href).length ||
+    (mainNav && href.split("/")[1] !== "" && href.split("/")[1] === item?.url?.split("/")[1]) ||
+    (!!item.url && href.startsWith(item.url + "/"))
   );
 }
