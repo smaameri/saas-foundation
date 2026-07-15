c06d4667c498d328df067100e7f48c4bd739aac4
Sami Maameri <ssmaameri@gmail.com>
2026-07-15 13:38:41 +0530

    update repo method to exclude unused fields

--- src/repositories/admin/userRepository.ts
+++ src/repositories/admin/userRepository.ts
@@ -1,22 +1,12 @@
 import { prisma } from "@/lib/prisma";
 
-export async function findById(id: string) {
-  return prisma.user.findUnique({
-    where: { id },
-  });
-}
-
-export async function updateUser(
-  id: string,
-  data: { firstName: string; lastName: string; image?: string | null },
-) {
+export async function updateUser(id: string, data: { firstName: string; lastName: string }) {
   return prisma.user.update({
     where: { id },
     data: {
       firstName: data.firstName,
       lastName: data.lastName,
       name: `${data.firstName} ${data.lastName}`,
-      image: data.image || null,
     },
   });
 }
