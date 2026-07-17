4257b08facaf77bacc6ac23bba328445d5490f89
Sami Maameri <ssmaameri@gmail.com>
2026-07-17 14:05:35 +0530

    remove portals column

---
+++ prisma/migrations/20260717083451/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `portals` on the `organization` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "organization" DROP COLUMN "portals";
