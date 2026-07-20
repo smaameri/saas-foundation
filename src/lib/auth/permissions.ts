bd4ea87c8682b0a22985027fc7a1ae043d241fbd
Sami Maameri <ssmaameri@gmail.com>
2026-07-20 13:51:23 +0530

    add org plugin permissions

--- src/lib/auth/permissions.ts
+++ src/lib/auth/permissions.ts
@@ -4,11 +4,17 @@ import { adminAc, defaultStatements, userAc } from "better-auth/plugins/admin/ac
 export const ac = createAccessControl({
   ...defaultStatements,
   apiKey: ["create", "read", "read:any", "update", "delete", "delete:any"] as const,
+  organization: ["update", "delete"] as const,
+  member: ["create", "update", "delete"] as const,
+  invitation: ["create", "cancel"] as const,
 });
 
 export const adminRole = ac.newRole({
   ...adminAc.statements,
   apiKey: ["create", "read", "read:any", "update", "delete", "delete:any"],
+  organization: ["update", "delete"],
+  member: ["create", "update", "delete"],
+  invitation: ["create", "cancel"],
 });
 
 export const userRole = ac.newRole({
@@ -17,7 +23,7 @@ export const userRole = ac.newRole({
 });
 
 /**
- * @example { member: ["create"], apiKey: ["read", "read:any"],  }
+ * @example { user: ["create"], apiKey: ["read", "read:any"],  }
  */
 export type Permissions = {
   [K in keyof typeof ac.statements]?: (typeof ac.statements)[K][number][];
